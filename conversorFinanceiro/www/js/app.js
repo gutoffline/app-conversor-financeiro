df464b195df768ef6737a46bf7430321ed2e745a
Guto Xavier <gutoffline@gmail.com>
2022-05-04 20:33:02 -0300

    função converter real para dolar

---
+++ conversorFinanceiro/www/js/app.js
@@ -0,0 +1,7 @@
+function ConverterRealDolar(){
+    let reais = document.querySelector('#reais').value
+    let dolar = 4.92
+    valorConvertido = reais / dolar
+    document.querySelector('#dolares').innerHTML = valorConvertido
+}
+
