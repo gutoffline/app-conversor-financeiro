c454f6e9910ee06c256d88a9b5dcf67feb88ac26
Guto Xavier <gutoffline@gmail.com>
2022-05-04 20:58:26 -0300

    ajax

--- conversorFinanceiro/www/js/app.js
+++ conversorFinanceiro/www/js/app.js
@@ -1,4 +1,26 @@
 function ConverterRealDolar(){
+/*
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: data,
+        success: success,
+        dataType: dataType
+      });
+*/
+    let api = 'https://economia.awesomeapi.com.br/last/USD'
+
+    $.ajax({
+        type: "GET",
+        url: api,
+        success: (function(){
+            console.log("DEU CERTO")
+        }),
+        beforeSend:(function(){
+            console.log("Processando")
+        })
+    })
+
     let reais = document.querySelector('#reais').value
     let dolar = 4.92
     valorConvertido = reais / dolar
