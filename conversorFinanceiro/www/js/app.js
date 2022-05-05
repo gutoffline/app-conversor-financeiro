5c12d4931761530812cd07e618e3a233e9c6648a
Guto Xavier <gutoffline@gmail.com>
2022-05-04 21:54:30 -0300

    funcionalidade de consultar a api e buscar o valor

--- conversorFinanceiro/www/js/app.js
+++ conversorFinanceiro/www/js/app.js
@@ -1,29 +1,15 @@
 function ConverterRealDolar(){
-/*
-    $.ajax({
-        type: "POST",
-        url: url,
-        data: data,
-        success: success,
-        dataType: dataType
-      });
-*/
+    let reais = document.querySelector('#reais').value
+    let dolar = 0
     let api = 'https://economia.awesomeapi.com.br/last/USD'
-
+    
     $.ajax({
         type: "GET",
         url: api,
-        success: (function(){
-            console.log("DEU CERTO")
-        }),
-        beforeSend:(function(){
-            console.log("Processando")
+        success: (function(retorno){
+            dolar = retorno.USDBRL.ask
+            valorConvertido = reais / dolar
+            document.querySelector('#dolares').innerHTML = valorConvertido
         })
     })
-
-    let reais = document.querySelector('#reais').value
-    let dolar = 4.92
-    valorConvertido = reais / dolar
-    document.querySelector('#dolares').innerHTML = valorConvertido
 }
-
